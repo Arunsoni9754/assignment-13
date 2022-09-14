87550776925cec8facadd5eae83cf292c5bcc3da
Arunsoni9754 <111188653+Arunsoni9754@users.noreply.github.com>
2022-09-14 21:54:41 +0530

    Create Q4.c

---
+++ Q4.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int sum(int);
+int main()
+{
+    int n;
+    printf("Enter the number of terms");
+    scanf("%d",&n);
+    int j=sum(n);
+    printf("The sum is %d",j);
+}
+int sum(int a)
+{
+    if(a==0)
+    {
+        return 0;
+    }
+    return (a*a+sum(a-1));
+}
